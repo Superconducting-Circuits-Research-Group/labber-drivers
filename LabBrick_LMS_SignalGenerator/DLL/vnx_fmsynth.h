6c7ac6eaf72276d745d96307e616d6871a21a05c
Ivan Pechenezhskiy <pecheneg@umd.edu>
2017-08-31 17:55:34 -0400

    Update the DLL library to the version on the flash stick

--- LabBrick_LMS_SignalGenerator/DLL/vnx_fmsynth.h
+++ LabBrick_LMS_SignalGenerator/DLL/vnx_fmsynth.h
@@ -3,28 +3,25 @@
 //	9/2010	RD	Updated version of DLL API definitions	
 // 
 //	3/2011	RD	Updated with new pulse mode status functions, ANSI-C version
+//
+//	6/2016	RD	Updated with new GetAbsPowerLevel and GetDLLVersion functions
+//
+// ------ ANSI-C Calling Convention --------------------------------------
 
-
-
-// ------ C++ Calling Convention --------
-#define VNX_FSYNSTH_API __declspec(dllimport)
+#define VNX_LMS_API __declspec(dllimport)
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
 
 // ----------- Global Equates ------------
 #define MAXDEVICES 64
 #define MAX_MODELNAME 32
 
 // ----------- Data Types ----------------
-
 #define DEVID unsigned int
 
-
 // ----------- Mode Bit Masks ------------
-
 #define MODE_RFON	0x00000010			// bit is 1 for RF on, 0 if RF is off
 #define MODE_INTREF	0x00000020			// bit is 1 for internal osc., 0 for external reference
 #define MODE_SWEEP	0x0000000F			// bottom 4 bits are used to keep the sweep control bits				
@@ -33,12 +30,8 @@ extern "C" {
 #define MODE_EXTPWM	0x00000200			// 1 for ext. PWM input enabled
 #define PWM_MASK	0x00000300
 
-// ----------- Command Equates -----------
-
-
 // Status returns for commands
 #define LVSTATUS int
-
 #define STATUS_OK 0
 #define BAD_PARAMETER 0x80010000		// out of range input -- frequency outside min/max etc.
 #define BAD_HID_IO    0x80020000
@@ -63,61 +56,52 @@ extern "C" {
 #define DEV_LOCKED	  0x00002000			// used internally by the DLL
 #define DEV_RDTHREAD  0x00004000			// used internally by the DLL
 
+// ----- functions -----
+VNX_LMS_API void fnLMS_SetTestMode(bool testmode);
+VNX_LMS_API int fnLMS_GetDLLVersion();
+VNX_LMS_API int fnLMS_GetNumDevices();
+VNX_LMS_API int fnLMS_GetDevInfo(DEVID *ActiveDevices);
+VNX_LMS_API int fnLMS_GetModelName(DEVID deviceID, char *ModelName);
+VNX_LMS_API int fnLMS_InitDevice(DEVID deviceID);
+VNX_LMS_API int fnLMS_CloseDevice(DEVID deviceID);
+VNX_LMS_API int fnLMS_GetSerialNumber(DEVID deviceID);
 
-VNX_FSYNSTH_API void fnLMS_SetTestMode(bool testmode);
-VNX_FSYNSTH_API int fnLMS_GetNumDevices();
-VNX_FSYNSTH_API int fnLMS_GetDevInfo(DEVID *ActiveDevices);
-VNX_FSYNSTH_API int fnLMS_GetModelName(DEVID deviceID, char *ModelName);
-VNX_FSYNSTH_API int fnLMS_InitDevice(DEVID deviceID);
-VNX_FSYNSTH_API int fnLMS_CloseDevice(DEVID deviceID);
-VNX_FSYNSTH_API int fnLMS_GetSerialNumber(DEVID deviceID);
+VNX_LMS_API LVSTATUS fnLMS_SetFrequency(DEVID deviceID, int frequency);
+VNX_LMS_API LVSTATUS fnLMS_SetStartFrequency(DEVID deviceID, int startfrequency);
+VNX_LMS_API LVSTATUS fnLMS_SetEndFrequency(DEVID deviceID, int endfrequency);
+VNX_LMS_API LVSTATUS fnLMS_SetSweepTime(DEVID deviceID, int sweeptime);
+VNX_LMS_API LVSTATUS fnLMS_SetPowerLevel(DEVID deviceID, int powerlevel);
+VNX_LMS_API LVSTATUS fnLMS_SetRFOn(DEVID deviceID, bool on);
+VNX_LMS_API LVSTATUS fnLMS_SetPulseOnTime(DEVID deviceID, float pulseontime);
+VNX_LMS_API LVSTATUS fnLMS_SetPulseOffTime(DEVID deviceID, float pulseofftime);
+VNX_LMS_API LVSTATUS fnLMS_EnableInternalPulseMod(DEVID deviceID, bool on);
+VNX_LMS_API LVSTATUS fnLMS_SetUseExternalPulseMod(DEVID deviceID, bool external);
+VNX_LMS_API LVSTATUS fnLMS_SetFastPulsedOutput(DEVID deviceID, float pulseontime, float pulsereptime, bool on);
+VNX_LMS_API LVSTATUS fnLMS_SetUseInternalRef(DEVID deviceID, bool internal);
+VNX_LMS_API LVSTATUS fnLMS_SetSweepDirection(DEVID deviceID, bool up);
+VNX_LMS_API LVSTATUS fnLMS_SetSweepMode(DEVID deviceID, bool mode);
+VNX_LMS_API LVSTATUS fnLMS_SetSweepType(DEVID deviceID, bool swptype);
+VNX_LMS_API LVSTATUS fnLMS_StartSweep(DEVID deviceID, bool go);
+VNX_LMS_API LVSTATUS fnLMS_SaveSettings(DEVID deviceID);
 
-
-VNX_FSYNSTH_API LVSTATUS fnLMS_SetFrequency(DEVID deviceID, int frequency);
-
-VNX_FSYNSTH_API LVSTATUS fnLMS_SetStartFrequency(DEVID deviceID, int startfrequency);
-VNX_FSYNSTH_API LVSTATUS fnLMS_SetEndFrequency(DEVID deviceID, int endfrequency);
-VNX_FSYNSTH_API LVSTATUS fnLMS_SetSweepTime(DEVID deviceID, int sweeptime);
-
-VNX_FSYNSTH_API LVSTATUS fnLMS_SetPowerLevel(DEVID deviceID, int powerlevel);
-VNX_FSYNSTH_API LVSTATUS fnLMS_SetRFOn(DEVID deviceID, bool on);
-
-VNX_FSYNSTH_API LVSTATUS fnLMS_SetPulseOnTime(DEVID deviceID, float pulseontime);
-VNX_FSYNSTH_API LVSTATUS fnLMS_SetPulseOffTime(DEVID deviceID, float pulseofftime);
-VNX_FSYNSTH_API LVSTATUS fnLMS_EnableInternalPulseMod(DEVID deviceID, bool on);
-VNX_FSYNSTH_API LVSTATUS fnLMS_SetUseExternalPulseMod(DEVID deviceID, bool external);
-VNX_FSYNSTH_API LVSTATUS fnLMS_SetFastPulsedOutput(DEVID deviceID, float pulseontime, float pulsereptime, bool on);
-
-VNX_FSYNSTH_API LVSTATUS fnLMS_SetUseInternalRef(DEVID deviceID, bool internal);
-VNX_FSYNSTH_API LVSTATUS fnLMS_SetSweepDirection(DEVID deviceID, bool up);
-VNX_FSYNSTH_API LVSTATUS fnLMS_SetSweepMode(DEVID deviceID, bool mode);
-VNX_FSYNSTH_API LVSTATUS fnLMS_SetSweepType(DEVID deviceID, bool swptype);
-VNX_FSYNSTH_API LVSTATUS fnLMS_StartSweep(DEVID deviceID, bool go);
-VNX_FSYNSTH_API LVSTATUS fnLMS_SaveSettings(DEVID deviceID);
-
-VNX_FSYNSTH_API int fnLMS_GetFrequency(DEVID deviceID);
-VNX_FSYNSTH_API int fnLMS_GetStartFrequency(DEVID deviceID);
-VNX_FSYNSTH_API int fnLMS_GetEndFrequency(DEVID deviceID);
-VNX_FSYNSTH_API int fnLMS_GetSweepTime(DEVID deviceID);
-
-VNX_FSYNSTH_API int fnLMS_GetRF_On(DEVID deviceID);
-VNX_FSYNSTH_API int fnLMS_GetUseInternalRef(DEVID deviceID);
-VNX_FSYNSTH_API int fnLMS_GetPowerLevel(DEVID deviceID);
-VNX_FSYNSTH_API int fnLMS_GetMaxPwr(DEVID deviceID);
-VNX_FSYNSTH_API int fnLMS_GetMinPwr(DEVID deviceID);
-VNX_FSYNSTH_API int fnLMS_GetMaxFreq(DEVID deviceID);
-VNX_FSYNSTH_API int fnLMS_GetMinFreq(DEVID deviceID);
-
-
-VNX_FSYNSTH_API float fnLMS_GetPulseOnTime(DEVID deviceID);
-VNX_FSYNSTH_API float fnLMS_GetPulseOffTime(DEVID deviceID);
-
-VNX_FSYNSTH_API int fnLMS_GetPulseMode(DEVID deviceID);
-
-VNX_FSYNSTH_API int fnLMS_GetHasFastPulseMode(DEVID deviceID);
-VNX_FSYNSTH_API int fnLMS_GetUseInternalPulseMod(DEVID deviceID);
-
-VNX_FSYNSTH_API int fnLMS_GetDeviceStatus(DEVID deviceID);
+VNX_LMS_API int fnLMS_GetFrequency(DEVID deviceID);
+VNX_LMS_API int fnLMS_GetStartFrequency(DEVID deviceID);
+VNX_LMS_API int fnLMS_GetEndFrequency(DEVID deviceID);
+VNX_LMS_API int fnLMS_GetSweepTime(DEVID deviceID);
+VNX_LMS_API int fnLMS_GetRF_On(DEVID deviceID);
+VNX_LMS_API int fnLMS_GetUseInternalRef(DEVID deviceID);
+VNX_LMS_API int fnLMS_GetPowerLevel(DEVID deviceID);
+VNX_LMS_API int fnLMS_GetAbsPowerLevel(DEVID deviceID);
+VNX_LMS_API int fnLMS_GetMaxPwr(DEVID deviceID);
+VNX_LMS_API int fnLMS_GetMinPwr(DEVID deviceID);
+VNX_LMS_API int fnLMS_GetMaxFreq(DEVID deviceID);
+VNX_LMS_API int fnLMS_GetMinFreq(DEVID deviceID);
+VNX_LMS_API float fnLMS_GetPulseOnTime(DEVID deviceID);
+VNX_LMS_API float fnLMS_GetPulseOffTime(DEVID deviceID);
+VNX_LMS_API int fnLMS_GetPulseMode(DEVID deviceID);
+VNX_LMS_API int fnLMS_GetHasFastPulseMode(DEVID deviceID);
+VNX_LMS_API int fnLMS_GetUseInternalPulseMod(DEVID deviceID);
+VNX_LMS_API int fnLMS_GetDeviceStatus(DEVID deviceID);
 
 #ifdef __cplusplus
 	}
