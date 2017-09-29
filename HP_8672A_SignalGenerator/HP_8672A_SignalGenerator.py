ca6ded172da2f6fba6059e2e9af1a5b0264087ae
patzinak <pechenezhski@wisc.edu>
2017-09-29 17:52:01 -0400

    Add the HP 8672A signal generator

---
+++ HP_8672A_SignalGenerator/HP_8672A_SignalGenerator.py
@@ -0,0 +1,21 @@
+from VISA_Driver import VISA_Driver
+
+
+class Driver(VISA_Driver):
+    """This class implements HP 8672A Signal Generator driver."""
+
+    def performSetValue(self, quant, value, sweepRate=0.0, options={}):
+        """Perform the Set Value instrument operation. This function
+        should return the actual value set by the instrument."""
+        # check quantity name
+        if quant.name == 'Frequency':
+            self.writeAndLog('P%08.fZ0' % (value / 1000.))
+        else:
+            # otherwise, call standard VISA case
+            VISA_Driver.performSetValue(self, quant, value, sweepRate,
+                    options)
+        return value
+
+
+if __name__ == '__main__':
+	pass
