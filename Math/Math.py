963a131188c8499b63405c2feda246826ce34fb4
patzinak <pechenezhski@wisc.edu>
2017-12-10 01:12:27 -0500

    Add the Math driver

---
+++ Math/Math.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+import numpy as np
+
+import InstrumentDriver
+
+
+class Driver(InstrumentDriver.InstrumentWorker):
+    """ This class implements a demodulation driver"""
+    def performOpen(self, options={}):
+        """Perform the operation of opening the instrument connection"""
+        pass
+
+    def performClose(self, bError=False, options={}):
+        """Perform the close instrument connection operation"""
+        pass
+
+    def performSetValue(self, quant, value, sweepRate=0.0, options={}):
+        """Perform the Set Value instrument operation. This function should
+        return the actual value set by the instrument"""
+        # do nothing here
+        return value
+        
+    def pairwiseDifference(self, traces):
+        return np.diff(traces)[:,0]
+        
+    def pairwiseRatio(self, traces):
+        return traces[:,0] / traces[:,1]
+
+    def performGetValue(self, quant, options={}):
+        """Perform the Get Value instrument operation"""
+        if quant.name in ('Difference',
+                          'Ratio',
+                          'Pairwise difference',
+                          'Pairwise ratio'):
+            if quant.name.startswith('Pairwise'):
+                data = self.getValue('Input vector')
+            else:
+                data = self.getValue('Input pair')
+            traces = data['y']
+            traces.shape = (-1, 2)
+            if quant.name.lower().endswith('difference'):
+                value = self.pairwiseDifference(traces)
+            else:
+                value = self.pairwiseRatio(traces)
+            if quant.name.startswith('Pairwise'):
+                data['y'] = value
+                value = data
+            else:
+                value = value[0]
+        else:
+            # just return the quantity value
+            value = quant.getValue()
+        return value
+
+if __name__ == '__main__':
+    pass
