27ba1fb8543eeeef7aa7c4e9fb0b1dd5fff1e548
rmencia <31523892+rmencia@users.noreply.github.com>
2019-07-19 19:14:31 -0400

    Fix the power issue

--- HP_8672A_SignalGenerator/HP_8672A_SignalGenerator.py
+++ HP_8672A_SignalGenerator/HP_8672A_SignalGenerator.py
@@ -9,7 +9,7 @@ class Driver(VISA_Driver):
         should return the actual value set by the instrument."""
         # check quantity name
         if quant.name == 'Frequency':
-            self.write('P%08.fZ0' % (value / 1000.))
+            self.write('P%08.fZ0K0L3M0N6O3' % (value / 1000.))
         else:
             # otherwise, call standard VISA case
             VISA_Driver.performSetValue(self, quant, value, sweepRate,
