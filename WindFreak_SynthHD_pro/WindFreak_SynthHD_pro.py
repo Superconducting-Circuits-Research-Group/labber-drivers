3d124e41e2d3b48488eb4cadf90b71e9876ac02e
xionghn13 <xionghn13@gmail.com>
2021-11-30 14:37:21 -0500

    Revert "Revert "Merge branch 'master' of https://github.com/Superconducting-Circuits-Research-Group/labber-drivers""
    This reverts commit de99ce387b0e004daba973eabd52880700a24852.

--- WindFreak_SynthHD_pro/WindFreak_SynthHD_pro.py
+++ WindFreak_SynthHD_pro/WindFreak_SynthHD_pro.py
@@ -1,17 +1,28 @@
 from VISA_Driver import VISA_Driver
 
-
 class Driver(VISA_Driver):
 
     def performSetValue(self, quant, value, sweepRate=0.0, options={}):
         """Perform the Set Value instrument operation. This function
         should return the actual value set by the instrument."""
         # check quantity name
-        if quant.name == 'Frequency':
+        if quant.name == 'FrequencyA':
+            self.writeAndLog('C0')
             self.writeAndLog('f%f' % (1.e-6 * value))
-        elif quant.name == 'Power':
+        elif quant.name == 'PowerA':
+            self.writeAndLog('C0')
             self.writeAndLog('W%f' % value)
-        elif quant.name == 'Output':
+        elif quant.name == 'OutputA':
+            self.writeAndLog('C0')
+            self.writeAndLog('r%d' % value)
+        elif quant.name == 'FrequencyB':
+            self.writeAndLog('C1')
+            self.writeAndLog('f%f' % (1.e-6 * value))
+        elif quant.name == 'PowerB':
+            self.writeAndLog('C1')
+            self.writeAndLog('W%f' % value)
+        elif quant.name == 'OutputB':
+            self.writeAndLog('C1')
             self.writeAndLog('r%d' % value)
         else:
             # otherwise, call standard VISA case
@@ -21,10 +32,26 @@ class Driver(VISA_Driver):
 
     def performGetValue(self, quant, options={}):
         """Perform the Get Value instrument operation."""
-        if quant.name == 'Frequency':
+        if quant.name == 'FrequencyA':
+            self.writeAndLog('C0')
             value = 1.e6 * float(self.ask('f?'))
-        elif quant.name == 'Power':
+        elif quant.name == 'PowerA':
+            self.writeAndLog('C0')
             value = self.ask('W?')
+        elif quant.name == 'OutputA':
+            self.writeAndLog('C0')
+            value = self.ask('r?')
+            value = int(value)
+        elif quant.name == 'FrequencyB':
+            self.writeAndLog('C1')
+            value = 1.e6 * float(self.ask('f?'))
+        elif quant.name == 'PowerB':
+            self.writeAndLog('C1')
+            value = self.ask('W?')
+        elif quant.name == 'OutputB':
+            self.writeAndLog('C1')
+            value = self.ask('r?')
+            value = int(value)
         else:
             # otherwise, call standard VISA case
             value = VISA_Driver.performGetValue(self, quant, options)
